fcc75b46b3823304efbe4039f41ef06119e5d31e
musasazib <musasazib@gmail.com>
2021-09-17 09:23:09 +0600

    overall summary

---
+++ summary.js
@@ -0,0 +1,17 @@
+alert('');
+confirm('Are you single');
+prompt('Name');
+
+// location.reload()
+// location.href = ''
+// location.hash = 
+
+// http://127.0.0.1:5500/shopping/shopping.html
+
+// history.back()
+// history.go()
+// history.forward()
+
+// cookies
+// sessionStorage Vs localStorage
+// cookies Vs sessionStorage Vs localStorage
